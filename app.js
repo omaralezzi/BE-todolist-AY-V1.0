e9de895722f2a22da8bbd19deef8346bd0a1c31e
Omar Al-Ezzi <ishtaar.it@gmail.com>
2021-12-07 21:33:39 +0100

    update app.js and views/list

--- app.js
+++ app.js
@@ -2,16 +2,44 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => {
-  // res.send('Hello World!');
+// need to be alway below const app = express();
+app.set("view engine", "ejs"); // set up ejs for templating and view engine as ejs
 
+app.get("/", (req, res) => {
   let today = new Date();
   let currentDay = today.getDay();
+  let day = "";
   if (currentDay === 6 || currentDay === 0) {
-    res.send("Yaaaa ...Today is weekend!");
+    day = "Weekend 😃";
   } else {
-    res.send("Nooo Way..Today is weekday!");
+    day = "Weekday 😉";
   }
+  switch (currentDay) {
+    case 0:
+      dayOfWeek = "Sunday";
+      break;
+    case 1:
+      dayOfWeek = "Monday";
+      break;
+    case 2:
+      dayOfWeek = "Tuesday";
+      break;
+    case 3:
+      dayOfWeek = "Wednesday";
+      break;
+    case 4:
+      dayOfWeek = "Thursday";
+      break;
+    case 5:
+      dayOfWeek = "Friday";
+      break;
+    case 6:
+      dayOfWeek = "Saturday";
+      break;
+  }
+  console.log(dayOfWeek);
+
+  res.render("list", { kindOfDay: day, dayOfWeekName: dayOfWeek });
 });
 
 app.listen(3000, () => {
