52833112c4cf5ec2a0e65751f8c6def3f0c405ad
Omar Al-Ezzi <ishtaar.it@gmail.com>
2021-12-07 18:58:33 +0100

    update app.js

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.get('/', (req, res) => {
-    // res.send('Hello World!');
-    
-    let today = new Date();
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send('Yaaaa ...Today is weekend!');
-    } else {
-        res.send('Nooo Way..Today is weekday!');
-    }
+app.get("/", (req, res) => {
+  // res.send('Hello World!');
+
+  let today = new Date();
+  let currentDay = today.getDay();
+  if (currentDay === 6 || currentDay === 0) {
+    res.send("Yaaaa ...Today is weekend!");
+  } else {
+    res.send("Nooo Way..Today is weekday!");
+  }
 });
 
-    
-
-
-
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-}   );
+  console.log("Example app listening on port 3000!");
+});
